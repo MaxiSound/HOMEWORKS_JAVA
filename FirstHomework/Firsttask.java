85a704b0d80eee08648385e414f67fc71667abbd
MaxiSound <maxisound@yandex.ru>
2022-12-12 17:48:11 +0700

    ready

--- FirstHomework/Firsttask.java
+++ FirstHomework/Firsttask.java
@@ -5,11 +5,11 @@ public class Firsttask {
         Scanner iScanner = new Scanner(System.in);
         System.out.printf("Введите первое число: ");
         int i = iScanner.nextInt();
-        System.out.printf("Треугольное число: %d\n", giveMeNumber(i));
+        System.out.printf("Треугольное число: %d\n", Number(i));
         iScanner.close();
     }
 
-    public static int giveMeNumber(int a) {
+    public static int Number(int a) {
         return (a * (a + 1)) / 2;
     }
 }
