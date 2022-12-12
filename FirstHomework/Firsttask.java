32a47be43ce030f7ad14f7bc6c101b5ae4747aeb
MaxiSound <maxisound@yandex.ru>
2022-12-12 17:46:46 +0700

    ready

--- FirstHomework/Firsttask.java
+++ FirstHomework/Firsttask.java
@@ -1,18 +1,15 @@
-/**
- * Firsttask
- * 1. Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! 
- **/
+import java.util.Scanner;
 
-public static void main(String[] args)
-{
-    Scanner iScanner = new Scanner(System.in);
-    System.out.printf("Введите первое число: ");
-    int i = iScanner.nextInt();
-    System.out.printf("Треугольное число: %d\n", giveMeNumber(i));
-    iScanner.close();
-}
+public class Firsttask {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите первое число: ");
+        int i = iScanner.nextInt();
+        System.out.printf("Треугольное число: %d\n", giveMeNumber(i));
+        iScanner.close();
+    }
 
-public static int giveMeNumber(int a) 
-{
-    return (a * (a + 1)) / 2;
+    public static int giveMeNumber(int a) {
+        return (a * (a + 1)) / 2;
+    }
 }
