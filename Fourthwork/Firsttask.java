57cbf3463d2cffcacc347d8330f87cd11eb667b3
MaxiSound <maxisound@yandex.ru>
2022-12-22 12:19:44 +0700

    ready

--- Fourthwork/Firsttask.java
+++ Fourthwork/Firsttask.java
@@ -1,7 +1,28 @@
 package Fourthwork;
 
-
+import java.util.ArrayDeque;
 
 public class Firsttask {
-    
+    public static ArrayDeque<Integer> getReversDeque(int count) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        ArrayDeque<Integer> reverseDeque = new ArrayDeque<>();
+
+        for (int i = 0; i < count; i++) {
+            int rand = (int) (Math.random() * (100 - 1));
+            deque.addFirst(rand);
+        }
+        System.out.println(deque);
+
+        for (int i = 0; i < count; i++) {
+            reverseDeque.addFirst(deque.getFirst());
+            deque.removeFirst();
+        }
+        return reverseDeque;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getReversDeque(10));
+
+    }
 }
